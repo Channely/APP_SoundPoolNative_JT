cfcda7d3ec7e9edcf604e2bb0a39300ab69a316b
Channely <qijie29896@gmail.com>
2013-09-11 13:57:23 +0800

    ccy - add js code for new plugin

--- app/assets/www/lib/js/infrastructure/native.js
+++ app/assets/www/lib/js/infrastructure/native.js
@@ -17,5 +17,13 @@ NativeAccess.prototype.send_sms = function (receivers, message_content, success_
     }, "MoodeSMS", "send_sms", [receivers, message_content]);
 };
 
+NativeAccess.prototype.play_voice = function (a, b, success_callback, error_callback, c) {
+    this.base_access.exec(function (result) {
+        success_callback.call(c, result);
+    }, function (err) {
+        error_callback.call(c);
+    }, "MoodeSP", "play_voice", [a, b]);
+};
+
 
 
--- www/js/plugins/sms.js
+++ www/js/plugins/sms.js
@@ -9,6 +9,10 @@ var native_accessor = {
         if (typeof this.process_received_message === 'function') {
             this.process_received_message(json_message);
         }
+    },
+
+    play_voice: function(){
+        native_access.play_voice();
     }
 
 };
@@ -19,4 +23,9 @@ function notify_message_received(message_json) {
     //alert(JSON.stringify(message_json.messages));
 //    native_accessor.receive_message(message_json);
     //phone_number=message_json.messages[0].phone;
+}
+
+function sound_out(){
+    native_accessor.play_voice();
+
 }
